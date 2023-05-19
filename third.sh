7b6c208ef9ccd681914b1fa699a7fde0a65d3dd0
devopsfaisal <clumsyfaisal@gmail.com>
2023-05-19 12:37:26 +0530

    Added third.sh

---
+++ third.sh
@@ -0,0 +1,13 @@
+# builtin, keywords, commands
+
+uptime
+
+type -a uptime
+
+type -a echo
+
+type -a if
+
+echo $PATH
+
+# If executable script or cmd in not available in builtin shell then it will check at echo $PATH
