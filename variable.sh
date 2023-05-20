85b9e302b746653444e81b9e40bd646ad83c60b4
devopsfaisal <clumsyfaisal@gmail.com>
2023-05-20 19:49:46 +0530

    Added variable.sh of ubuntu

--- variable.sh
+++ variable.sh
@@ -1,4 +1,30 @@
-name=lateef
-age=20
+#!/bin/bash
 
-echo "My name is ${name} and my age is ${age}
+# user define variable
+
+name="Faisal"
+age=30
+
+echo "My name is ${name} and my age is ${age}"
+
+echo 'My name is ${name} and my age is ${age}'
+
+work=" programming"
+space="shell"
+
+echo "This is $space$work"
+# system define variable
+
+echo $HOME
+echo ${SHELL}
+echo ${PATH}
+echo ${OSTYPE}
+echo ${$}
+echo ${PPID}
+
+
+echo "My name is faisal"
+
+sleep 30
+
+# ps -ef | grep "variable.sh" To check the pid
